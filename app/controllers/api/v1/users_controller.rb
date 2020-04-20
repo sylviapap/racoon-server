f699e6c61f93fd64e338201a7dc82d2864c197a7
Sylvia <sylviapap1@gmail.com>
2020-04-20 16:05:30 -0700

    all routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :find_user, only: [:update, :show, :destroy]
+    # skip_before_action :authorized, only: [:create]
+
+    def index
+      @users = User.all
+      render json: @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+  
+    def new
+    end
+  
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = issue_token(@user)
+            render json: { user: UserSerializer.new(@user), token: @token }, status: :created
+        else
+            render json: { errors: @user.errors.full_messages }, status: :not_acceptable
+        end
+    end
+
+    def edit
+    end
+  
+    def update
+      @user.update(user_params)
+      if @user.save
+        render json: @user, status: :accepted
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:username, :password)
+    end
+  
+    def find_user
+      @user = User.find(params[:id])
+    end
 end
