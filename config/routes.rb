1888be0d03d336682b9e4d3825c205ac9be45cfa
Sylvia <sylviapap1@gmail.com>
2020-04-21 11:02:57 -0700

    added new models

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :user_events
+      resources :map_events
+      resources :comments
+
       post '/auth', to: 'auth#create'
       get '/current_user', to: 'auth#show'
+      post '/profile', to: 'user#profile'
+      
     end
   end
 end
