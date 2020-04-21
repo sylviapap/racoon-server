ea5c66cbca514ee12e779900ae60e1fa06bed56f
Sylvia <sylviapap1@gmail.com>
2020-04-21 10:57:34 -0700

    relationships

--- app/models/map_event.rb
+++ app/models/map_event.rb
@@ -1,2 +1,5 @@
 class MapEvent < ApplicationRecord
+    has_many :user_events
+    has_many :comments
+    has_many :users, through: :user_events
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
 
+    has_many :comments, dependent: :destroy
+    has_many :user_events, dependent: :destroy 
+    has_many :map_events, through: :user_events
+
 end
