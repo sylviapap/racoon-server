552e10112e16078d35aac413593597e1c9bb1e09
Sylvia <sylviapap1@gmail.com>
2020-04-21 11:11:46 -0700

    basic controllers

--- app/controllers/api/v1/map_events_controller.rb
+++ app/controllers/api/v1/map_events_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::MapEventsController < ApplicationController
+    before_action :find_event, only: [:show, :update]
+
+    def index
+        map_events = MapEvent.all
+        render json: map_events
+    end 
+
+    def show
+        comments = map_event.comments
+        render json: map_event, :include => [:users, :comments  => {:include => :user }]
+    end 
+
+    def create
+        map_event = MapEvent.create(map_event_params)
+        render json: map_event
+    end 
+
+    def update
+        map_event.update(map_event_params)
+        render json: map_event, :include => [:users => {:include => :map_events}]
+    end 
+
+    private
+    
+    def map_event_params
+        params.require(:map_event)
+        .permit(:title, 
+                :address, 
+                :latitude, 
+                :longitude)
+    end 
+
+    def query_params
+        params.permit(:title)
+    end 
+
+    def find_event
+        map_event = MapEvent.find(params[:id])
+    end
 end
--- app/controllers/api/v1/user_events_controller.rb
+++ app/controllers/api/v1/user_events_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::UserEventsController < ApplicationController
+    def index
+        user_events = UserEvent.where(params[:user_id])
+        render json: user_events
+    end 
+
+    def show
+        user_event = UserEvent.find(params[:id])
+    end 
+
+    def create
+        user_event = UserEvent.create(user_event_params)
+        map_event = user_event.map_event
+        user = user_event.user
+        map_events = user.map_events
+
+        if user_event.valid?
+            render json: user_event, :include => [:user => {:include => :map_events}]
+        else
+            render json: { error: 'failed to add event' }, status: :not_acceptable
+        end
+    end 
+
+    private
+
+    def user_event_params
+        params.require(:user_event).permit(:user_id, :map_event_id)
+    end 
 end
