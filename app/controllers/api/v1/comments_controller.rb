727f528dfb7416789136c2fcec3f1999aee9e8be
Sylvia <sylviapap1@gmail.com>
2020-04-21 11:00:48 -0700

    basic controllers

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::CommentsController < ApplicationController
+    before_action :find_comment, only: [:show, :destroy]
+    
+    def index
+        comments = Comment.all 
+        render json: comments
+    end 
+
+    def show
+        render json: comment
+    end 
+
+    def create
+        comment = Comment.create(comment_params)
+        render json: comment
+    end 
+
+    def destroy
+    end 
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:user_id, :community_event_id, :content)
+    end 
+
+    def find_comment
+        comment = Comment.find(params[:id])
+    end
 end
