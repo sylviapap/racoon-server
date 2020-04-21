ac6689c70f54c27efffe15d898a3c8a50a77fd31
Sylvia <sylviapap1@gmail.com>
2020-04-21 10:28:20 -0700

    seeds, faker

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem "jwt", "~> 2.1"
  
 gem "active_model_serializers", "~> 0.10.7"
 
+gem "faker", "~> 1.9"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-
 User.destroy_all
-User.create(username: "Mary", email: "mary@mail.com", password: "password")
-User.create(username: "John", password: "password")
-User.create(username: "Jane", password: "password")
+
+3.times do
+    User.create(username: Faker::Name.first_name, password: "pw", email: Faker::Internet.email)
+end
