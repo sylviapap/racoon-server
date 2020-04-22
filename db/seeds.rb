95d6d89153c5da50fd05e783bd14d097975bccf0
Sylvia <sylviapap1@gmail.com>
2020-04-22 10:29:36 -0700

    renaming, db reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@ Comment.destroy_all
     User.create(username: Faker::Name.first_name, password: "pw", email: Faker::Internet.email)
 end
 
-5.times do
+10.times do
     MapMarker.create(latitude: Faker::Address.latitude, longitude: Faker::Address.longitude, address: Faker::Address.street_address, title: Faker::Address.community)
 end
 
-3.times do
+10.times do
     Comment.create(content: Faker::Quote.singular_siegler, user_id: User.all.sample.id, map_marker_id: MapMarker.all.sample.id)
 end
 
-3.times do
+5.times do
     UserMarker.create(user_id: User.all.sample.id, map_marker_id: MapMarker.all.sample.id)
 end
