5e8ac1e1bce3bee237e6fca4e20d492c9596e177
Sylvia <sylviapap1@gmail.com>
2020-04-23 10:10:06 -0700

    comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,27 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }
 
     has_many :comments, dependent: :destroy
-    has_many :user_markers, dependent: :destroy 
+
+    has_many :user_markers
     has_many :map_markers, through: :user_markers
 
 end
+
+# 
+# class Author < ApplicationRecord
+#     has_many :books, inverse_of: 'writer'
+#   end
+   
+#   class Book < ApplicationRecord
+#     belongs_to :writer, class_name: 'Author', foreign_key: 'author_id'
+#   end
+
+# Bookmark 
+#  belongs_to :post
+#  belongs_to :user
+# User
+#  has_many :posts
+#  has_many :posts, :through => :bookmarks
+# Post
+#  belongs_to :user
+#  has_many :posts, :through => :bookmarks
--- app/models/user_marker.rb
+++ app/models/user_marker.rb
@@ -2,3 +2,9 @@ class UserMarker < ApplicationRecord
   belongs_to :user
   belongs_to :map_marker
 end
+
+# 
+# class Book < ApplicationRecord
+#   belongs_to :author, class_name: "Patron",
+#                         foreign_key: "patron_id"
+# end
