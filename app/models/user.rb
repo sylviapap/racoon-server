233763548c625bb10c0751a5c52c91195e2332e0
Sylvia <sylviapap1@gmail.com>
2020-04-23 11:15:44 -0700

    removed comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,24 +11,3 @@ class User < ApplicationRecord
     has_many :created_markers, class_name: 'MapMarker', inverse_of: 'creator'
 
 end
-
-# 
-# class Author
-#     has_many :books, inverse_of: 'writer'
-# end
-   
-# class Book
-#     belongs_to :writer, class_name: 'Author', foreign_key: 'author_id'
-# end
-
-# User
-    # has_many :bookmarks
-    # has_many :posts, through: :bookmarks
-    # has_many :authored_posts, foreign_key: :author_id, class_name: 'Post'
-# Bookmark
-    # belongs_to :post
-    # belongs_to :user
-# Post
-    # belongs_to :author, class_name: 'User'
-    # has_many :bookmarks
-    # has_many :users, through: :bookmarks
--- app/models/user_marker.rb
+++ app/models/user_marker.rb
@@ -2,9 +2,3 @@ class UserMarker < ApplicationRecord
   belongs_to :user
   belongs_to :map_marker
 end
-
-# 
-# class Book < ApplicationRecord
-#   belongs_to :author, class_name: "Patron",
-#                         foreign_key: "patron_id"
-# end
