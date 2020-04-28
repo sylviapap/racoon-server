32081f21020521429a3448886677dbd4c3457548
Sylvia <sylviapap1@gmail.com>
2020-04-27 22:58:38 -0700

    added destroy

--- app/controllers/api/v1/map_markers_controller.rb
+++ app/controllers/api/v1/map_markers_controller.rb
@@ -22,6 +22,11 @@ class Api::V1::MapMarkersController < ApplicationController
         render json: map_marker, :include => [:users => {:include => :map_markers}]
     end
 
+    def destroy
+        map_marker = MapMarker.find(params[:id])
+        map_marker.destroy
+    end
+
     private
     
     def map_marker_params
