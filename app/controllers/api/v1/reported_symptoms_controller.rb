ccca5213e3b20cad165206c44fb47b021f9890a4
Sylvia <sylviapap1@gmail.com>
2020-04-29 10:19:27 -0700

    added routes

--- app/controllers/api/v1/reported_symptoms_controller.rb
+++ app/controllers/api/v1/reported_symptoms_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::ReportedSymptomsController < ApplicationController
+  def index
+    reported_symptoms = ReportedSymptom.where(params[:user_id])
+    render json: reported_symptoms
+  end 
+
+  def show
+    reported_symptom = ReportedSymptom.find(params[:id])
+  end 
+
+  def create
+    reported_symptom = ReportedSymptom.find_or_create_by(reported_symptom_params)
+    render json: reported_symptom
+  end 
+
+  def destroy
+    reported_symptom = ReportedSymptom.find(params[:id])
+    reported_symptom.destroy
+  end
+
+  private
+
+  def reported_symptom_params
+    params.permit(:user_id, :symptom_id)
+  end
 end
