7ac008f82b57a6bb31630db48a0a21177e3e9998
Sylvia <sylviapap1@gmail.com>
2020-04-29 11:10:32 -0700

    symptom seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
     has_many :created_markers, class_name: 'MapMarker', inverse_of: 'creator', dependent: :destroy
 
-    has_many :reported_symptoms
-    has_many :symptoms, through: :reported_symptoms
+    has_many :reported_symptoms, dependent: :destroy
+    has_many :symptoms, through: :reported_symptoms, dependent: :destroy
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,25 @@
 require 'rest-client'
 require 'json'
+require 'dotenv-rails'
 
-# headers = {"App-Id": ENV["INFERMEDICA_API_ID"], "App-Key": ENV["INFERMEDICA_API_KEY"], "Model": "infermedica-en", "Version": "v3"}
+# APIheaders = {"App-Id" => "#{ENV["INFERMEDICA_APP_ID"]}", "App-Key" => "#{ENV["INFERMEDICA_APP_KEY"]}"}
 
-# result = RestClient.get("https://api.infermedica.com/covid19/symptoms", headers=headers)
+# result = RestClient.get("https://api.infermedica.com/covid19/symptoms", headers=APIheaders)
 # symptoms_array = JSON.parse(result)
-# symptoms = symptoms_array.each do |object|
-#     Symptom.create()
+# symptoms_array.each do |object|
+#     Symptom.create(
+#         category: object["category"],
+#         common_name: object["common_name"],
+#         infermedica_id: object["id"],
+#         name: object["name"],
+#         question: object["question"],
+#         seriousness: object["seriousness"],
+#         sex_filter: object["sex_filter"])
 #     end
 
-User.destroy_all
-MapMarker.destroy_all
-Bookmark.destroy_all
-Comment.destroy_all
+# User.destroy_all
+# MapMarker.destroy_all
+# Comment.destroy_all
 
 5.times do
     User.create(username: Faker::Name.first_name, password: "pw", email: Faker::Internet.email)
@@ -23,9 +30,5 @@ end
 end
 
 10.times do
-    Comment.create(content: Faker::Marketing.buzzwords, user_id: User.all.sample.id, map_marker_id: MapMarker.all.sample.id)
+    Comment.create(content: "this is an example comment", user_id: User.all.sample.id, map_marker_id: MapMarker.all.sample.id)
 end
-
-# 5.times do
-#     Bookmark.create(user_id: User.all.sample.id, map_marker_id: MapMarker.all.sample.id)
-# end
