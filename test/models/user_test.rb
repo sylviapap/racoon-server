819e4efaf676de6825f0a395492ca856c0a8eeef
Sylvia <sylviapap1@gmail.com>
2020-07-29 11:37:21 -0700

    Add testing boilerplate

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without email" do
+    user = User.new
+    user.first_name = "tester"
+    user.last_name = "mctest"
+    user.password = "password"
+    user.password_confirmation = "password"
+    assert_not user.save, "Saved the user without email"
+   end
 end
