6f4ab83e2a540035a9b5e4f9ed2334bf39a9f770
Sylvia <sylviapap1@gmail.com>
2020-10-30 18:26:41 -0700

    Remove comment

--- app/models/map_marker.rb
+++ app/models/map_marker.rb
@@ -1,7 +1,6 @@
 class MapMarker < ApplicationRecord
     has_many :bookmarks, dependent: :destroy
     has_many :comments, dependent: :destroy
-    # has_many :users, through: :bookmarks, dependent: :destroy
     has_many :users, through: :comments, dependent: :destroy
 
     belongs_to :creator, class_name: 'User', foreign_key: :user_id
